5806dc39e57d02eaf2fa493a8ca932a7bc5892c1
Carol Chung <cch5ng@gmail.com>
2020-11-23 18:54:21 -0800

    9 google auth (#10)
    * add pg pkg; stub out sql for basic tables
    
    * stub out the env var file
    
    * stub out sample env var files
    
    * init files and also stub out files for quick google login
    
    * add helper that requests new auth tokens before expiration, 11 hr
    
    * add @auth0/auth0-react pkg; config related to auth0
    
    * start swapping out the old google-react-login with auth0 login
    
    * cleanup
    
    * add dotenv config
    
    * add dotenv pkg
    
    * add custom hook for making api requests client to server
    
    * add packages for BE auth verification; configure express and add test routes to check for auth validation and scope check
    
    * fix the ref to checkJwt
    
    * revert to old audience config but there is some issue with getting the jwt with correct audience
    
    * add react router and remove old google-react-login
    
    * set up react auth using latest tutorial through end of protecting routes
    
    * update typo
    
    * add cors
    
    * update test for make api request from client to server with auth token

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,62 @@
+require('dotenv').config();
 var logger = require('morgan');
 var express = require('express');
+const cors = require("cors");
 var cookieParser = require('cookie-parser');
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
 var indexRouter = require('./routes/index');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors({ origin: process.env.CLIENT_ORIGIN_URL }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+// Authorization middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and 
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'http://localhost:3000',
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+//app.use(checkJwt);
 app.use('/v1', indexRouter);
+
+// This route doesn't need authentication
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
+});
+
+// This route needs authentication
+app.get('/api/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});
+
+const checkScopes = jwtAuthz([ 'read:companies' ]);
+
+app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:companies to see this.'
+  });
+});
 
 module.exports = app;
