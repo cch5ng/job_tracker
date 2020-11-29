7d24412f6f2260737551629924c88f3e3ab13105
Carol Chung <cch5ng@gmail.com>
2020-11-29 13:12:19 -0800

    change role to job

--- server/src/bin/sql/job.sql
+++ server/src/bin/sql/job.sql
@@ -1,10 +1,12 @@
-CREATE TABLE role(
+CREATE TABLE job(
   id SERIAL PRIMARY KEY,
   name VARCHAR(64),
   source VARCHAR(255),
+  url VARCHAR(350),
   description VARCHAR(5000),
-  questions_self VARCHAR(1000),
+  questions VARCHAR(1000),
   status VARCHAR(64),
   company_id INT,
+  company_name VARCHAR(255),
   FOREIGN KEY (company_id) REFERENCES company(id)
 );
