d8874365df1ad93eafdef0094003663e84e6f973
Carol Chung <cch5ng@gmail.com>
2020-12-01 19:35:48 -0800

    move the checkJwt function to utils; try to add a check for 401 resp but not sure handled correctly

--- server/src/api/job.js
+++ server/src/api/job.js
@@ -2,38 +2,41 @@ const { Router } = require('express');
 const JobTable = require('../tables/job');
 const CompanyTable = require('../tables/company');
 const AuthTable = require('../tables/auth');
+const {checkJwt} = require('../utils');
 
 const router = Router();
 
 //create new job
 //should take into consideration creating a new company (vs an existing company)
 //for now just create a new company
-router.post('/', (req, res, next) => {
+router.post('/', checkJwt, (req, res, next) => {
   const {name, status, description, url, company_name, questions, source, user_guid} = req.body;
-
-  // AuthTable.getUserGuid({email: user_email})
-  //   .then(resp => {
-  //     let {user_guid} = resp;
-      if (user_guid) {
-        JobTable.postJob({name, status, description, url, company_name, questions, source, user_guid})
-        .then(({message}) => {
-          res.status(201).json({message})
-        })
-        .catch(error => next(error))    
-      } else {
-        res.status(200).json({message: 'Could not save job because there is an issue with the current user email authorization'})
-      }
-    // })
-    // .catch(err => next(err))
+    if (user_guid) {
+      JobTable.postJob({name, status, description, url, company_name, questions, source, user_guid})
+      .then(resp => {
+        if (resp.status_code === 401) {
+          res.status(401).json({error: 'Please log in and try again.'})
+        } else {
+          res.status(201).json(resp)
+        }
+      })
+      .catch(error => next(error))    
+    } else {
+      res.status(200).json({message: 'Could not save job because there is an issue with the current user email authorization'})
+    }
 });
 
-router.put('/update/:job_guid', (req, res, next) => {
+router.put('/update/:job_guid', checkJwt, (req, res, next) => {
   const guid = req.params.job_guid;
   const {name, status, description, url, company_name, questions, source, user_guid} = req.body;
   if (user_guid) {
     JobTable.updateJob({name, status, description, url, company_name, questions, source, user_guid, guid})
-    .then(({message}) => {
-      res.status(201).json({message})
+    .then(resp => {
+      if (resp.status_code === 401) {
+        res.status(401).json({error: 'Please log in and try again.'})
+      } else {
+        res.status(201).json(resp)
+      }
     })
     .catch(error => next(error))    
   } else {
@@ -42,18 +45,30 @@ router.put('/update/:job_guid', (req, res, next) => {
 });
 
 //retrieve all jobs (later search/filter)
-router.get('/:user_guid', (req, res, next) => {
+router.get('/:user_guid', checkJwt, (req, res, next) => {
   const {user_guid} = req.params;
   JobTable.getJobs({user_guid})
-    .then(resp => res.status(200).json(resp))
+    .then(resp => {
+      if (resp.status_code === 401) {
+        res.status(401).json({error: 'Please log in and try again.'})
+      } else {
+        res.status(200).json(resp)
+      }
+    })
     .catch(err => next(err));
 })
 
 //update job given jobId to set to status archived (maybe reuse the generic put route?)
-router.put('/archive/:job_guid', (req, res, next) => {
+router.put('/archive/:job_guid', checkJwt, (req, res, next) => {
   const {job_guid} = req.params;
   JobTable.archiveJob({job_guid})
-    .then(resp => res.status(200).json(resp))
+    .then(resp => {
+      if (resp.status_code === 401) {
+        res.status(401).json({error: 'Please log in and try again.'})
+      } else {
+        res.status(200).json(resp)
+      }
+    })
     .catch(err => next(err));
 })
 
