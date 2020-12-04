7928330a13ef5515237c133963b845e7569120f6
Carol Chung <cch5ng@gmail.com>
2020-12-04 14:18:04 -0800

    modify event table schema

--- server/src/bin/sql/event.sql
+++ server/src/bin/sql/event.sql
@@ -1,10 +1,12 @@
 CREATE TABLE event(
   id SERIAL PRIMARY KEY,
   name VARCHAR(64),
+  format VARCHAR(64),
   contact VARCHAR(255),
   notes VARCHAR(1000),
   description VARCHAR(5000),
+  follow_up VARCHAR(500),
   date_time TIMESTAMPTZ(64),
-  role_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id)
+  job_id INT,
+  FOREIGN KEY (job_id) REFERENCES job(id)
 );
