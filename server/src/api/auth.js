2de8f25b82e55fcf85eaa906c20dafdc4156d7a9
Carol Chung <cch5ng@gmail.com>
2020-12-04 21:46:19 -0800

    refactor auth api

--- server/src/api/auth.js
+++ server/src/api/auth.js
@@ -3,14 +3,21 @@ const AuthTable = require('../tables/auth');
 
 const router = Router();
 
-router.post('/', (req, res, next) => {
-  const {email} = req.body;
-  AuthTable.postUser({email})
-    .then(resp => {
-      res.status(200).json(resp)
-    })
-    .catch(error => next(error))
-});
+// router.post('/', (req, res, next) => {
+//   const {email} = req.body;
+//   AuthTable.getUserByEmail({email})
+//     .then(resp => {
+//       if (resp.error) {
+//         AuthTable.postUser({email})
+//         .then(resp => {
+  
+//           res.status(200).json(resp)
+//         })
+//         .catch(error => next(error))  
+//       }
+//     })
+//     .catch(error => next(error))
+// });
 
 router.post('/guid', (req, res, next) => {
   const {email} = req.body;
@@ -20,5 +27,13 @@ router.post('/guid', (req, res, next) => {
     })
     .catch(error => next(error))
 })
+
+router.post('/user', (req, res, next) => {
+  const {email} = req.body;
+  AuthTable.postUser({email})
+    .then(resp => resp.json())
+    .then(json => res.status(200).json(resp))
+    .catch(error => next(error))  
+});
 
 module.exports = router;
