a720cc1cf145381466c2a3b508b52ffd46176401
Carol Chung <cch5ng@gmail.com>
2020-12-07 18:33:17 -0800

    refactor to branch display between events for a job vs events for a user

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -1,47 +1,58 @@
-import {useParams} from 'react-router-dom';
+import {useParams, Link} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import {useAppAuth} from '../../context/auth-context';
-import {getDictFromAr, getArFromDict, convertISOStrToLocalDateTime} from '../../utils';
+import {getDictFromAr, getArFromDictBasic, convertISOStrToLocalDateTime, orderArByProp} from '../../utils';
 
 function Events(props) {
   const {jobId} = useParams();
   const [eventsDict, setEventsDict] = useState({});
-  const {sessionToken} = useAppAuth();
+  const {userGuid, sessionToken} = useAppAuth();
 
   useEffect(() => {
-    let url = `http://localhost:3000/api/events/job/${jobId}`;
-    fetch(url, {
-      headers: {Authorization: `Bearer ${sessionToken}`}
-    })
-      .then(resp => resp.json())
-      .then(json => {
-        if (json.events.length) {
-          let evDict = json.events ? getDictFromAr(json.events): {};
-          setEventsDict(evDict);
-        } 
+    let url;
+    if (!jobId && userGuid) {
+      url = `http://localhost:3000/api/events/user/${userGuid}`;
+    } else if (jobId) {
+      url = `http://localhost:3000/api/events/job/${jobId}`
+    }
+    if (url) {
+      fetch(url, {
+        headers: {Authorization: `Bearer ${sessionToken}`}
       })
-      .catch(err => console.error('error', err))
+        .then(resp => resp.json())
+        .then(json => {
+          if (json.events.length) {
+            let evDict = json.events ? getDictFromAr(json.events): {};
+            setEventsDict(evDict);
+          } 
+        })
+        .catch(err => console.error('error', err))
+  
+    }
   }, [])
 
-  let eventsAr = Object.keys(eventsDict).length ? getArFromDict(eventsDict) : [];
-  console.log('eventsAr', eventsAr)
+  let eventsAr = Object.keys(eventsDict).length ? getArFromDictBasic(eventsDict) : [];
+  orderArByProp(eventsAr, 'follow_up', 'desc');
 
   return (
     <div>
-      <div>TODO EVENTS LIST</div>
-      { eventsAr.map(event => (
+      <h1>EVENTS</h1>
+      { eventsAr.map(event => {
+        let url = `/events/edit/${event.guid}`;
+        return (
           <div key={event.guid}>
-            <div>name: {event.name}</div>
-            <div>format: {event.format}</div>
-            <div>contact: {event.contact}</div>
-            <div>notes: {event.notes}</div>
-            <div>description: {event.description}</div>
-            <div>follow up: {event.follow_up}</div>
-            <div>date/time: {convertISOStrToLocalDateTime(event.date_time)}</div>
-
+            <Link to={url}>
+              <div>name: {event.name}</div>
+              <div>format: {event.format}</div>
+              <div>contact: {event.contact}</div>
+              <div>notes: {event.notes}</div>
+              <div>description: {event.description}</div>
+              <div>follow up: {event.follow_up}</div>
+              <div>date/time: {convertISOStrToLocalDateTime(event.date_time)}</div>
+            </Link>
           </div>
-      ))}
-
+        )
+      })}
     </div>
   );
 };
