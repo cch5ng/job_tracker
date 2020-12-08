5363bfbf63b179b143d52d7729807f79f88e1e44
Carol Chung <cch5ng@gmail.com>
2020-12-08 12:22:02 -0800

    add delete button to events list view

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -8,6 +8,29 @@ function Events(props) {
   const [eventsDict, setEventsDict] = useState({});
   const {userGuid, sessionToken} = useAppAuth();
 
+  const buttonOnClickHandler = (ev) => {
+    ev.preventDefault();
+    let {id, name} = ev.target;
+    let eventGuid = name;
+    fetch(`http://localhost:3000/api/events/${eventGuid}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionToken}`
+      }
+    })
+    .then(resp => resp.json())
+    .then(json => {
+      if (json.status === 'success') {
+        let copyEventDict = {...eventsDict};
+        delete copyEventDict[eventGuid];
+        setEventsDict(copyEventDict);
+      }
+    })
+    .catch(err => console.error('err', err))
+
+  }
+
   useEffect(() => {
     let url;
     if (!jobId && userGuid) {
@@ -34,13 +57,17 @@ function Events(props) {
   let eventsAr = Object.keys(eventsDict).length ? getArFromDictBasic(eventsDict) : [];
   orderArByProp(eventsAr, 'follow_up', 'desc');
 
+  let createUrl = `/events/new/${jobId}`;
+
   return (
     <div>
       <h1>EVENTS</h1>
+      <Link to={createUrl}>Add Event</Link>
       { eventsAr.map(event => {
         let url = `/events/edit/${event.guid}`;
         return (
           <div key={event.guid}>
+            <button id="buttonDelete" name={event.guid} onClick={buttonOnClickHandler}>Delete</button>
             <Link to={url}>
               <div>name: {event.name}</div>
               <div>format: {event.format}</div>
