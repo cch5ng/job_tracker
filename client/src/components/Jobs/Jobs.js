55ef0bf8f76a3d69734a8295fea4354b49e93f8d
Carol Chung <cch5ng@gmail.com>
2020-12-17 11:32:38 -0800

    style grid for jobs list

--- client/src/components/Jobs/Jobs.js
+++ client/src/components/Jobs/Jobs.js
@@ -66,38 +66,40 @@ function Jobs() {
   orderArByProp(jobsAr, 'created_at', 'desc')
 
   return (
-    <div className={styles.jobs_container}>
+    <div className={styles.jobs_container_outter}>
       <h1 className={styles.view_title}>JOBS LIST</h1>
       <Link to="/jobs/new" className={styles.link_icon}>
         <div className={styles.add_icon}>+</div>
       </Link>
 
-      {jobsAr.map(job => {
-        let url = `/jobs/${job.guid}`;
-        let newEventUrl = `events/new/${job.guid}`;
-        let eventsUrl = `jobs/${job.guid}/events`;
-        return (
-          <div key={job.guid} className={styles.job_container}>
-            <Link to={url}>
-              <h2>{job.name}</h2>
-              <div><span className={styles.label}>status</span> {job.status}</div>
-              <div><span className={styles.label}>company</span> {job.name}</div>
-              <div><span className={styles.label}>url</span> {job.url}</div>
-              <div><span className={styles.label}>description</span> {job.description}</div>
-              <div><span className={styles.label}>questions</span> {job.questions}</div>
-              <div><span className={styles.label}>source</span> {job.source}</div>
-              {job.created_at && (
-                <div className={styles.small}>created {job.created_at}</div>
-              )}
-            </Link>
-            <div className={styles.button_container}>
-              <Link to={newEventUrl}><button className={styles.nav_button}>Add event</button></Link>
-              <Link to={eventsUrl}><button className={styles.nav_button}>Get events</button></Link>
-              <button onClick={handleArchiveButtonClick} name={job.guid} className={styles.button}>Hide</button>
+      <div className={styles.jobs_container} >
+        {jobsAr.map(job => {
+          let url = `/jobs/${job.guid}`;
+          let newEventUrl = `events/new/${job.guid}`;
+          let eventsUrl = `jobs/${job.guid}/events`;
+          return (
+            <div key={job.guid} className={styles.job_container}>
+              <Link to={url}>
+                <h2>{job.name}</h2>
+                <div><span className={styles.label}>status</span> {job.status}</div>
+                <div><span className={styles.label}>company</span> {job.name}</div>
+                <div><span className={styles.label}>url</span> {job.url}</div>
+                <div><span className={styles.label}>description</span> {job.description}</div>
+                <div><span className={styles.label}>questions</span> {job.questions}</div>
+                <div><span className={styles.label}>source</span> {job.source}</div>
+                {job.created_at && (
+                  <div className={styles.small}>created {job.created_at}</div>
+                )}
+              </Link>
+              <div className={styles.button_container}>
+                <Link to={newEventUrl}><button className={styles.nav_button}>Add event</button></Link>
+                <Link to={eventsUrl}><button className={styles.nav_button}>Get events</button></Link>
+                <button onClick={handleArchiveButtonClick} name={job.guid} className={styles.button}>Hide</button>
+              </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
     </div>
   );
 };
