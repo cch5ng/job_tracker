15ee3ca3fa94fbd66b2238357f28855893f66a05
Carol Chung <cch5ng@gmail.com>
2020-12-22 16:03:17 -0800

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import {useState} from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import {AuthProvider} from './context/auth-context';
 import Home from './components/Home';
-// import Login from './components/Login';
-// import Logout from './components/Logout';
 
 function App() {
   const { isAuthenticated } = useAuth0();
@@ -12,7 +10,6 @@ function App() {
     <AuthProvider>
       <div id="app" className="d-flex flex-column h-100">
         <Home />
-        {/* <Footer /> */}
       </div>
     </AuthProvider>
   );
