b719101fccbd0555ff786f92eac13af05b07fe9d
Carol Chung <cch5ng@gmail.com>
2020-12-25 18:45:22 -0800

    fix syntax for label and missing key values

--- client/src/components/FormShared/Input.js
+++ client/src/components/FormShared/Input.js
@@ -15,7 +15,7 @@ const Input = ({type, value, name, inputOnChangeHandler, label, inline}) => {
   return (
     <div className={inputGroupClassName}>
       {label && (
-        <label for={name} className={styles.label}>{label}</label>
+        <label htmlFor={name} className={styles.label}>{label}</label>
       )}
       <input type={type} value={value} name={name} 
         className={styles.input} onChange={ev => inputOnChangeHandler(ev)}/>
