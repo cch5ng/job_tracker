f8a4a2ae952139194a45af44130d663d6d38cba2
Carol Chung <cch5ng@gmail.com>
2020-12-26 21:15:27 -0800

    refactor to use checkbox or other input

--- client/src/components/FormShared/Input.js
+++ client/src/components/FormShared/Input.js
@@ -3,7 +3,7 @@ import styles from './Input.module.css';
 
 let cx = classNames.bind(styles);
 
-const Input = ({type, value, name, inputOnChangeHandler, label, inline}) => {
+const Input = ({type, value, name, inputOnChangeHandler, label, inline, checked}) => {
 
   const inputGroupClassName = cx(
     {
@@ -17,8 +17,14 @@ const Input = ({type, value, name, inputOnChangeHandler, label, inline}) => {
       {label && (
         <label htmlFor={name} className={styles.label}>{label}</label>
       )}
-      <input type={type} value={value} name={name} 
-        className={styles.input} onChange={ev => inputOnChangeHandler(ev)}/>
+      {checked !== undefined && (
+        <input type={type} checked={checked} name={name} 
+          className={styles.input} onChange={ev => inputOnChangeHandler(ev)}/>
+      )}
+      {value !== undefined && (
+        <input type={type} value={value} name={name} 
+          className={styles.input} onChange={ev => inputOnChangeHandler(ev)}/>
+      )}
     </div>
   )
 }
