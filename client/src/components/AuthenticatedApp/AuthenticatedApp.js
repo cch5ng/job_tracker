5beebe9cdad96c3f72523c924aea1b7ff912dd20
Carol Chung <cch5ng@gmail.com>
2021-01-03 15:45:04 -0800

    try revert to old logic before create jobs context

--- client/src/components/AuthenticatedApp/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp/AuthenticatedApp.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect
 } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import {useAppAuth} from '../../context/auth-context';
 import AuthenticatedNavBar from "../Header/AuthenticatedNavBar"; //, Footer, Loading 
 import Profile from '../Profile';
 import ProtectedRoute from "../../auth/ProtectedRoute";
@@ -15,24 +17,52 @@ import JobsFormEdit from '../Jobs/JobsFormEdit';
 import EventsFormCreate from '../Events/EventsFormCreate';
 import EventsFormEdit from '../Events/EventsFormEdit';
 import Events from '../Events/Events';
+import {JobsProvider, useJobs} from '../../context/jobs-context';
 
 const AuthenticatedApp = () => {
+  const {login, getUserGuid, userGuid, userEmail, sessionToken} = useAppAuth();
+  const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
+  const {getJobsForUser} = useJobs();
+  const { name, picture, email } = user;
+
+  // const getTokenAndStore = async (userGuid) => {
+  //   const token = await getAccessTokenSilently();
+  //   login({userEmail: email, sessionToken: token, userGuid})
+
+  //   let url;
+  //   if (userGuid) {
+  //     url = `http://localhost:3000/api/jobs/all/${userGuid}`;
+  //   } 
+  //   if (url && token) {
+  //     getJobsForUser({url, token})
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   getUserGuid({userEmail: email})
+  //     .then(uGuid => {
+  //       getTokenAndStore(uGuid);
+  //     })
+  // }, [])
+
   return (
     <div>
       <AuthenticatedNavBar />
       <main className="main">
-        <Switch>
-          {/* <Route path="/" exact component={Home} /> */}
-          <ProtectedRoute path="/profile" component={Profile} />
-          <ProtectedRoute path="/external-api" component={ExternalApi} />
-          <ProtectedRoute exact path="/jobs/new" component={JobsFormCreate} />
-          <ProtectedRoute exact path="/jobs/:jobId/events" component={Events} />
-          <ProtectedRoute exact path="/jobs/:jobId" component={JobsFormEdit} />
-          <ProtectedRoute path="/jobs" component={Jobs} />
-          <ProtectedRoute exact path="/events/new/:jobId" component={EventsFormCreate} />
-          <ProtectedRoute exact path="/events/edit/:eventId" component={EventsFormEdit} />
-          <ProtectedRoute exact path="/events" component={Events} />
-        </Switch>
+        {/* <JobsProvider> */}
+          <Switch>
+            {/* <Route path="/" exact component={Home} /> */}
+            <ProtectedRoute path="/profile" component={Profile} />
+            <ProtectedRoute path="/external-api" component={ExternalApi} />
+            <ProtectedRoute exact path="/jobs/new" component={JobsFormCreate} />
+            <ProtectedRoute exact path="/jobs/:jobId/events" component={Events} />
+            <ProtectedRoute exact path="/jobs/:jobId" component={JobsFormEdit} />
+            <ProtectedRoute exact path="/events/new/:jobId" component={EventsFormCreate} />
+            <ProtectedRoute exact path="/events/edit/:eventId" component={EventsFormEdit} />
+              <ProtectedRoute path="/jobs" component={Jobs} />
+              <ProtectedRoute exact path="/events" component={Events} />
+          </Switch>
+        {/* </JobsProvider> */}
       </main>
     </div>
     )
