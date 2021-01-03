7b800b140ce7bfc76889988c18b8073d93118460
Carol Chung <cch5ng@gmail.com>
2021-01-03 15:42:41 -0800

    comment out job provider for now

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import AuthenticatedApp from './AuthenticatedApp/AuthenticatedApp';
 import UnauthenticatedApp from './UnauthenticatedApp/UnauthenticatedApp';
+// import {JobsProvider} from '../context/jobs-context';
+
 
 function Home() {
   const { isAuthenticated } = useAuth0();
