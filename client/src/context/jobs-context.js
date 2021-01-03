8ef6f23374936e6c7c412ce76456772167a171ac
Carol Chung <cch5ng@gmail.com>
2021-01-03 15:42:15 -0800

    try refactor get jobs

--- client/src/context/jobs-context.js
+++ client/src/context/jobs-context.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {getDictFromAr} from '../utils';
 
 const JobsContext = React.createContext([{}, () => {}]);
 function JobsProvider({children}) {
@@ -7,62 +8,42 @@ function JobsProvider({children}) {
     status: 'pending'
   });
 
-  // const getJobsByUserGuid = ({userGuid}) => {
-  //   //make api request
-  //   //check if the user exists in the db
-  //   return fetch('http://localhost:3000/api/auth/guid', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({email: userEmail})
-  //   })
-  //     .then(resp => resp.json())
-  //     .then(json => {
-  //       if (json.user_guid) {
-  //         //yes
-  //         let userGuid = json.user_guid;
-  //         setState({...state, userGuid});
-  //         return userGuid;  
-  //       } else {
-  //         //no
-  //         return fetch('http://localhost:3000/api/auth/user', {
-  //           method: 'POST',
-  //           headers: {
-  //             'Content-Type': 'application/json'
-  //           },
-  //           body: JSON.stringify({email: userEmail})
-  //         })
-  //           .then(resp => resp.json())
-  //           .then(json => {
-  //             console.log('json', json)
-  //             if (json.user_guid) {
-  //               //yes
-  //               let userGuid = json.user_guid;
-  //               setState({...state, userGuid});
-  //               return userGuid;  
-  //             }
-  //           })
-  //       }
-  //     })
-  //     .catch(err => console.error('err', err))        
-  // }
+  const getJobsForUser = ({url, token}) => {
+    fetch(url, {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+      .then(resp => resp.json())
+      .then(json => {
+        if (json.jobs.length) {
+          let {jobs} = json;
+          let jobsObj = getDictFromAr(jobs);
+          setState({...state, jobsDict: jobsObj, status: 'success'})
+        } 
+      })
+      .catch(err => {
+        console.error('error', err);
+        setState({...state, status: 'error'})
+      })      
+  }
 
-  let jobsState = {...state, }; //getJobsByUserGuid
+  let jobsState = {...state, getJobsForUser}; //getJobsByUserGuid
 
   /**
    * Provider component is the place where you'd pass a prop called value to, 
    * which you can subsequently consume within the Consumer component
    */
-  return (
-    <JobsContext.Provider value={jobsState}>
+/*
       {state.status === 'pending' ? (
         'Loading...'
       ) : state.status === 'error' ? (
         'There was an error retrieving jobs. Please retry.'
       ) : (
-        children
       )}
+*/
+
+   return (
+    <JobsContext.Provider value={jobsState}>
+        {children}
     </JobsContext.Provider>
   )
 }
