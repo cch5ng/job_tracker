49a9853d06089d0ec402b9cc71b2d820c8597ff5
Carol Chung <cch5ng@gmail.com>
2021-01-30 22:12:32 -0800

    utils unit tests

--- client/src/utils/utils.test.js
+++ client/src/utils/utils.test.js
@@ -33,6 +33,12 @@ let eventsAr = [
   {guid: 9, date_time: '2021-05-09T00:00:00.000Z', description: 'test event9'}
 ];
 
+let eventsAr2 = [
+  {guid: 3, date_time: '2021-01-31T00:00:00.000Z', description: 'test event3'},
+  {guid: 6, date_time: '2021-01-03T00:00:00.000Z', description: 'test event6'},
+  {guid: 9, date_time: '2021-05-09T00:00:00.000Z', description: 'test event9'}
+];
+
 const eventsArChron = [
   {guid: 6, date_time: '2021-01-03T00:00:00.000Z', description: 'test event6'},
   {guid: 3, date_time: '2021-01-31T00:00:00.000Z', description: 'test event3'},
@@ -51,18 +57,41 @@ test('orderArByProp - asc', () => {
   expect(eventsAr).toEqual(eventsArChron);
 });
 
-test.skip('orderArByProp - desc', () => {
-  expect(orderArByProp(eventsAr, 'date_time', 'desc')).toEqual(eventsArBackChron);
+test('orderArByProp - desc', () => {
+  orderArByProp(eventsAr2, 'date_time', 'desc');
+  expect(eventsAr2).toEqual(eventsArBackChron);
 });
 
-const jobsAr2 = [
-
+let jobsAr2 = [
+  {guid: 1, created_at: '2021-05-09T00:00:00.000Z', description: 'test event9'},
+  {guid: 2, created_at: '2021-01-31T00:00:00.000Z', description: 'test event3'},
+  {guid: 3, created_at: '2021-01-03T00:00:00.000Z', description: 'test event6'}
 ];
 
-test.skip('prettyFormatDate - single digit', () => {
-  expect(prettyFormatDate(9)).toEqual('09');
+let jobsAr3 = [
+  {guid: 1, created_at: '2021-01-31T00:00:00.000Z', description: 'test event3'},
+  {guid: 2, created_at: '2021-01-03T00:00:00.000Z', description: 'test event6'},
+  {guid: 3, created_at: '2021-05-09T00:00:00.000Z', description: 'test event9'}
+];
+
+const jobsArChron = [
+  {guid: 3, created_at: '2021-01-03T00:00:00.000Z', description: 'test event6'},
+  {guid: 2, created_at: '2021-01-31T00:00:00.000Z', description: 'test event3'},
+  {guid: 1, created_at: '2021-05-09T00:00:00.000Z', description: 'test event9'}
+]
+
+const jobsArBackChron = [
+  {guid: 3, created_at: '2021-05-09T00:00:00.000Z', description: 'test event9'},
+  {guid: 1, created_at: '2021-01-31T00:00:00.000Z', description: 'test event3'},
+  {guid: 2, created_at: '2021-01-03T00:00:00.000Z', description: 'test event6'}  
+]
+
+test('orderArByProp - asc jobs', () => {
+  orderArByProp(jobsAr2, 'created_at', 'asc');
+  expect(jobsAr2).toEqual(jobsArChron);
 });
 
-test.skip('prettyFormatDate - double digit', () => {
-  expect(prettyFormatDate(28)).toEqual('28');
+test('orderArByProp - desc jobs', () => {
+  orderArByProp(jobsAr3, 'created_at', 'desc');
+  expect(jobsAr3).toEqual(jobsArBackChron);
 });
