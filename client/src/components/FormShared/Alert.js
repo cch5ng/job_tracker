745922ca7eb4251151425905e318de93b542e5cd
Carol Chung <cch5ng@gmail.com>
2021-02-19 15:45:37 -0800

    refactor to use updated alert context

--- client/src/components/FormShared/Alert.js
+++ client/src/components/FormShared/Alert.js
@@ -2,56 +2,67 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import { AiFillCloseCircle, AiOutlineClose } from "react-icons/ai";
 import styles from './Alert.module.css';
+import { useAlert, REMOVE } from '../../context/alert-context';
 
 let cx = classNames.bind(styles);
 
-const Alert = ({type, message, onClickHandler, id}) => {
-  const isAlertError = type === 'error';
+//type, message, onClickHandler, id
+const Alert = ({alert}) => {
+  const { alertDispatch } = useAlert();
 
-  let alertClassName = cx({
-    alert_error: isAlertError,
-    alert_success: !isAlertError,
-    alert: true,
-    row_global_top: true,
-    alert_container: true
-  });
-
-  let titleClassName = cx({
-    alert_title_error: isAlertError,
-    alert_title_success: !isAlertError,
-    title: true
-  });
-
-  let messageClassName = cx({
-    alert_message_error: isAlertError,
-    alert_message_success: !isAlertError
-  });
-
-  let iconLeftClassName = cx({
-    icon_error: isAlertError,
-    icon_success: !isAlertError,
-    icon_left: true
-  })
-
-  let iconRightClassName = cx({
-    icon_error: isAlertError,
-    icon_success: !isAlertError,
-    icon_right: true
-  })
-
+  //TODO put the parent div in here
+  //also need to put a mapping function in here because the prop will be a list of alert objects
   return (
-    <div className={alertClassName} onClick={(ev) => onClickHandler(ev)} id={id}>
-      <div className={iconLeftClassName}><AiFillCloseCircle /></div>
-      <div className={styles.alert_content}>
-        {type === 'error' && (
-          <p className={titleClassName}>Error</p>
-        )}
-        {type !== 'error' && (
-          <p className={titleClassName}>Sucess</p>
-        )}
-        <p className={messageClassName}>{message}</p>
-      </div>
-      <div className={iconRightClassName}><AiOutlineClose /></div>
+    <div id="alert_parent">
+      {alert.map(al => {
+        const {type, id, message} = al;
+        const isAlertError = type === 'error';
+
+        let alertClassName = cx({
+          alert_error: isAlertError,
+          alert_success: !isAlertError,
+          alert: true,
+          row_global_top: true,
+          alert_container: true
+        });
+        let titleClassName = cx({
+          alert_title_error: isAlertError,
+          alert_title_success: !isAlertError,
+          title: true
+        });
+        let messageClassName = cx({
+          alert_message_error: isAlertError,
+          alert_message_success: !isAlertError
+        });
+        let iconLeftClassName = cx({
+          icon_error: isAlertError,
+          icon_success: !isAlertError,
+          icon_left: true
+        });
+        let iconRightClassName = cx({
+          icon_error: isAlertError,
+          icon_success: !isAlertError,
+          icon_right: true
+        });
+
+        return (
+          <div className={alertClassName} onClick={() =>
+            alertDispatch({ type: REMOVE, payload: { id } })
+          } id={id}>
+            <div className={iconLeftClassName}><AiFillCloseCircle /></div>
+            <div className={styles.alert_content}>
+              {type === 'error' && (
+                <p className={titleClassName}>Error</p>
+              )}
+              {type !== 'error' && (
+                <p className={titleClassName}>Sucess</p>
+              )}
+              <p className={messageClassName}>{message}</p>
+            </div>
+            <div className={iconRightClassName}><AiOutlineClose /></div>
+          </div>
+        )
+      })}
     </div>
   )
 }
