e20888541452fa7366fa13ec43ce16ad6412d573
Carol Chung <cch5ng@gmail.com>
2021-02-19 10:16:36 -0800

    add alertCloseHandler()

--- client/src/components/AuthenticatedApp/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp/AuthenticatedApp.js
@@ -15,22 +15,43 @@ import JobsFormEdit from '../Jobs/JobsFormEdit';
 import EventsFormCreate from '../Events/EventsFormCreate';
 import EventsFormEdit from '../Events/EventsFormEdit';
 import Events from '../Events/Events';
+import Alert from '../FormShared/Alert';
+import {useAlert} from '../../context/alert-context';
 
 const AuthenticatedApp = () => {
   const [showNavMenu, setShowNavMenu] = useState(false);
+  const {getAlertDict, getAlertList, removeFromAlertDict} = useAlert();
+
+  //TEST
+  const alertList = getAlertList();
 
   const toggleNavMenuDisplay = (ev) => {
     const {className} = ev.target;
-    if ((className.indexOf('close_icon') === -1 || className.indexOf('hamburger') === -1) && showNavMenu) {
+    if (className && (className.indexOf('close_icon') === -1 || className.indexOf('hamburger') === -1) && showNavMenu) {
       setShowNavMenu(!showNavMenu);
-    } else if (className.indexOf('close_icon') > -1 || className.indexOf('hamburger') > -1) {
+    } else if (className && className.indexOf('close_icon') > -1 || className.indexOf('hamburger') > -1) {
       setShowNavMenu(!showNavMenu);
+    } else {
+      console.log('to handle');
     }
+  }
+
+  const alertCloseHandler = (ev) => {
+    const parentDiv = ev.target.closest('#alertParent');
+    const alertDiv = parentDiv.firstElementChild;
+    const alertId = alertDiv.id;
+    removeFromAlertDict(alertId);
   }
 
   return (
     <div>
       <AuthenticatedNavBar showNavMenu={showNavMenu} toggleNavMenuDisplay={toggleNavMenuDisplay} />
+      <div id="alertParent">
+        {alertList.map(alert => (
+          <Alert message={alert.message} onClickHandler={alertCloseHandler} id={alert.id} 
+            type={alert.type ? alert.type : `success`}/>
+        ))}
+      </div>
       <main className="main" onClick={toggleNavMenuDisplay}>
         <Switch>
           <ProtectedRoute path="/profile" component={Profile} />
