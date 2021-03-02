ea67c0d7890ff7128a3b34f478469fc2a37486a0
Carol Chung <cch5ng@gmail.com>
2021-03-02 11:07:45 -0800

    update jobs POST and PUT API response and form validation

--- server/src/api/job.js
+++ server/src/api/job.js
@@ -11,25 +11,47 @@ const router = Router();
 //for now just create a new company
 router.post('/', checkJwt, (req, res, next) => {
   const {name, status, description, url, company_name, questions, source, user_guid, guid, created_at} = req.body;
-    if (user_guid) {
-      JobTable.postJob({name, status, description, url, company_name, questions, source, user_guid, guid, created_at})
-      .then(resp => {
-        if (resp.status_code === 401) {
-          res.status(401).json({error: 'Please log in and try again.'})
-        } else {
-          res.status(201).json(resp)
-        }
-      })
-      .catch(error => next(error))    
-    } else {
-      res.status(200).json({message: 'Could not save job because there is an issue with the current user email authorization'})
+  const error_fields = [];
+  const required_fields = ['name', 'status', 'company_name', 'description', 'source'];
+
+  required_fields.forEach(field => {
+    if (!req.body[field] || req.body[field] === 'none') {
+      error_fields.push(field);
     }
+  });
+  if (error_fields.length) {
+    const error_message = `These fields are required: ${error_fields.join(', ')}`;
+    res.status(400).json({message: error_message, type: 'error'})
+  } else if (user_guid) {
+    JobTable.postJob({name, status, description, url, company_name, questions, source, user_guid, guid, created_at})
+    .then(resp => {
+      if (resp.status_code === 401) {
+        res.status(401).json({error: 'Please log in and try again.'})
+      } else {
+        res.status(201).json(resp)
+      }
+    })
+    .catch(error => next(error))    
+  } else {
+    res.status(200).json({message: 'Could not save job because there is an issue with the current user email authorization'})
+  }
 });
 
 router.put('/update/:job_guid', checkJwt, (req, res, next) => {
   const guid = req.params.job_guid;
   const {name, status, description, url, company_name, questions, source, user_guid} = req.body;
-  if (user_guid) {
+  const error_fields = [];
+  const required_fields = ['name', 'status', 'company_name', 'description', 'source'];
+
+  required_fields.forEach(field => {
+    if (!req.body[field] || req.body[field] === 'none') {
+      error_fields.push(field);
+    }
+  });
+  if (error_fields.length) {
+    const error_message = `These fields are required: ${error_fields.join(', ')}`;
+    res.status(400).json({message: error_message, type: 'error'})
+  } else if (user_guid) {
     JobTable.updateJob({name, status, description, url, company_name, questions, source, user_guid, guid})
     .then(resp => {
       if (resp.status_code === 401) {
