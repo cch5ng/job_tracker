d851c8556d3c2d3082701992bcb8bfb2987622e8
Carol Chung <cch5ng@gmail.com>
2021-03-24 10:33:30 -0700

    modify sql for company and job tables

--- server/src/bin/sql/company.sql
+++ server/src/bin/sql/company.sql
@@ -1,6 +1,7 @@
 CREATE TABLE company(
   id SERIAL PRIMARY KEY,
   name VARCHAR(64),
-  financial_notes VARCHAR(1000),
-  summary VARCHAR(5000)
+  financial VARCHAR(1000),
+  description VARCHAR(5000),
+  purpose VARCHAR(300)
 );
