98b1bf8820d45e3fb6fd4b0978069a25f0c1d460
Carol Chung <cch5ng@gmail.com>
2021-09-16 17:09:59 -0700

    switched format back to js because of ts error

---
+++ client/src/index.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import {BrowserRouter as Router} from "react-router-dom";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const audience = 'http://localhost:3000';
+//`https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`;
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+ReactDOM.render(
+  <Router>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Auth0Provider>
+  </Router>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
