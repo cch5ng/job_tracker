c86a794b4d34160e92d21d245148e5a703a8c5d5
Carol Chung <cch5ng@gmail.com>
2021-09-23 16:53:59 -0700

    add route for signup view

--- client/src/components/UnauthenticatedApp/UnauthenticatedApp.js
+++ client/src/components/UnauthenticatedApp/UnauthenticatedApp.js
@@ -9,16 +9,24 @@ import LoginButton from "../Header/LoginButton";
 import UnauthenticatedNavBar from '../Header/UnauthenticatedNavBar';
 import styles from './UnauthenticatedApp.module.css';
 import splashScreen from '../..//daoud-abismail-unsplash.jpg';
+import Signup from '../Signup/Signup';
 
 const UnauthenticatedApp = () => {
   return (
     <div>
       <UnauthenticatedNavBar />
       <main>
-        <div className={styles.splash_screen_container} >
-          <img src={splashScreen} className={styles.splash_screen} />
-          <LoginButton />
-        </div>
+        <Switch>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+          <Route exact path="/">
+            <div className={styles.splash_screen_container} >
+              <img src={splashScreen} className={styles.splash_screen} />
+              <LoginButton />
+            </div>
+          </Route>
+        </Switch>
       </main>
     </div>
   )
