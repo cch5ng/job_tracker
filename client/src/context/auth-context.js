0179581047fe3acc1240be83d104a72a38813d09
Carol Chung <cch5ng@gmail.com>
2021-09-27 17:11:44 -0700

    add user guid setter, fix some state change pointers

--- client/src/context/auth-context.js
+++ client/src/context/auth-context.js
@@ -1,23 +1,53 @@
 import React, { useState } from 'react';
+import { createBrowserHistory } from 'history';
 
 const AuthContext = React.createContext([{}, () => {}]);
 function AuthProvider({children}) {
-  const [state, setState] = useState({
-    userEmail: null,
-    userGuid: null,
-    sessionToken: null
+  const [curAuthState, setCurAuthState] = React.useState({
+    status: 'logged out',
+    error: null,
   });
+  const [user, setUser] = useState(null);
+  const [userGuid, setUserGuid] = useState('');
 
-  const login = ({userEmail, sessionToken, userGuid}) => {
-    setState({...state, userEmail, userGuid, sessionToken});
-  }
+  let history = createBrowserHistory();
 
   const logout = () => {
-    setState({
-      userEmail: null,
-      userGuid: null,
-      sessionToken: null
-    });
+    localStorage.removeItem('blackjackAuthToken');
+    setUser(null);
+    setCurAuthState({
+      status: 'logged out',
+      error: null,
+    })
+    history.push('/');
+  }
+
+  const login = () => {
+    setCurAuthState({
+      status: 'logged in',
+      error: null,
+    })
+  }
+
+  const setUserGuidReq = ({userEmail}) => {
+    //no
+    return fetch('http://localhost:3000/api/auth/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email: userEmail})
+    })
+      .then(resp => resp.json())
+      .then(json => {
+        if (json.user_guid) {
+          //yes
+          let userGuid = json.user_guid;
+          setUserGuid(userGuid);
+          login();
+          return userGuid;  
+        }
+      })
   }
 
   const getUserGuid = ({userEmail}) => {
@@ -35,33 +65,16 @@ function AuthProvider({children}) {
         if (json.user_guid) {
           //yes
           let userGuid = json.user_guid;
-          setState({...state, userGuid});
+          setUserGuid(userGuid);
           return userGuid;  
         } else {
-          //no
-          return fetch('http://localhost:3000/api/auth/user', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({email: userEmail})
-          })
-            .then(resp => resp.json())
-            .then(json => {
-              console.log('json', json)
-              if (json.user_guid) {
-                //yes
-                let userGuid = json.user_guid;
-                setState({...state, userGuid});
-                return userGuid;  
-              }
-            })
+          setUserGuidReq({userEmail})
         }
       })
       .catch(err => console.error('err', err))        
   }
 
-  let authState = {...state, logout, getUserGuid, login};
+  let authState = {status: curAuthState.status, logout, login, user, getUserGuid, setUserGuidReq}
 
   /**
    * Provider component is the place where you'd pass a prop called value to, 
@@ -69,9 +82,9 @@ function AuthProvider({children}) {
    */
   return (
     <AuthContext.Provider value={authState}>
-      {state.status === 'pending' ? (
+      {curAuthState.status === 'pending' ? (
         'Loading...'
-      ) : state.status === 'logged out' ? (
+      ) : curAuthState.status === 'logged out' ? (
         children
       ) : (
         children
@@ -81,7 +94,7 @@ function AuthProvider({children}) {
 }
 
 //this seems simpler method to pass functions from context to consumers
-function useAppAuth() {
+function useAuth() {
   const context = React.useContext(AuthContext)
   if (context === undefined) {
     throw new Error(`useAuth must be used within a AuthProvider`)
@@ -89,4 +102,56 @@ function useAppAuth() {
   return context;
 }
 
-export {AuthProvider, useAppAuth};
+export {AuthProvider, useAuth};
+
+// import React, { useState } from 'react';
+
+// const AuthContext = React.createContext([{}, () => {}]);
+// function AuthProvider({children}) {
+//   const [state, setState] = useState({
+//     userEmail: null,
+//     userGuid: null,
+//     sessionToken: null
+//   });
+
+//   const login = ({userEmail, sessionToken, userGuid}) => {
+//     setState({...state, userEmail, userGuid, sessionToken});
+//   }
+
+//   const logout = () => {
+//     setState({
+//       userEmail: null,
+//       userGuid: null,
+//       sessionToken: null
+//     });
+//   }
+
+//   let authState = {...state, logout, getUserGuid, login};
+
+//   /**
+//    * Provider component is the place where you'd pass a prop called value to, 
+//    * which you can subsequently consume within the Consumer component
+//    */
+//   return (
+//     <AuthContext.Provider value={authState}>
+//       {state.status === 'pending' ? (
+//         'Loading...'
+//       ) : state.status === 'logged out' ? (
+//         children
+//       ) : (
+//         children
+//       )}
+//     </AuthContext.Provider>
+//   )
+// }
+
+// //this seems simpler method to pass functions from context to consumers
+// function useAppAuth() {
+//   const context = React.useContext(AuthContext)
+//   if (context === undefined) {
+//     throw new Error(`useAuth must be used within a AuthProvider`)
+//   }
+//   return context;
+// }
+
+// export {AuthProvider, useAppAuth};
