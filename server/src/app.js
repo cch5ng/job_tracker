ccd9710c43da024f4672d727b5931d3c2cbe5236
Carol Chung <cch5ng@gmail.com>
2021-10-05 15:03:47 -0700

    cleanup comment auth0

--- server/src/app.js
+++ server/src/app.js
@@ -4,9 +4,10 @@ var express = require('express');
 const cors = require("cors");
 const path = require("path");
 var cookieParser = require('cookie-parser');
-const jwt = require('express-jwt');
-const jwtAuthz = require('express-jwt-authz');
-const jwksRsa = require('jwks-rsa');
+var admin = require('firebase-admin');
+// const jwt = require('express-jwt');
+// const jwtAuthz = require('express-jwt-authz');
+// const jwksRsa = require('jwks-rsa');
 var indexRouter = require('./api/index');
 var app = express();
 
@@ -31,22 +32,22 @@ app.use(cookieParser());
 // Authorization middleware. When used, the
 // Access Token must exist and be verified against
 // the Auth0 JSON Web Key Set
-const checkJwt = jwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and 
-  // the signing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-  }),
+// const checkJwt = jwt({
+//   // Dynamically provide a signing key
+//   // based on the kid in the header and 
+//   // the signing keys provided by the JWKS endpoint.
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+//   }),
 
-  // Validate the audience and the issuer.
-  audience: 'http://localhost:3000',
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256']
-});
+//   // Validate the audience and the issuer.
+//   audience: 'http://localhost:3000',
+//   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+//   algorithms: ['RS256']
+// });
 
 //app.use(checkJwt);
 
@@ -58,25 +59,25 @@ app.use('/api/company', companyRouter);
 app.use('/v1', indexRouter);
 
 // This route doesn't need authentication
-app.get('/api/public', function(req, res) {
-  res.json({
-    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
-  });
-});
+// app.get('/api/public', function(req, res) {
+//   res.json({
+//     message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+//   });
+// });
 
 // This route needs authentication
-app.get('/api/private', checkJwt, function(req, res) {
-  res.json({
-    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
-  });
-});
+// app.get('/api/private', checkJwt, function(req, res) {
+//   res.json({
+//     message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+//   });
+// });
 
-const checkScopes = jwtAuthz([ 'read:companies' ]);
+// const checkScopes = jwtAuthz([ 'read:companies' ]);
 
-app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
-  res.json({
-    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:companies to see this.'
-  });
-});
+// app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+//   res.json({
+//     message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:companies to see this.'
+//   });
+// });
 
 module.exports = app;
