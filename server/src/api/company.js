16083e327e7ca2916b388395182a1cd76ab88a82
Carol Chung <cch5ng@gmail.com>
2021-10-07 14:50:27 -0700

    refactor get to use post for passing fbIdToken

--- server/src/api/company.js
+++ server/src/api/company.js
@@ -1,14 +1,30 @@
 const { Router } = require('express');
+const admin = require("firebase-admin");
+
 const CompanyTable = require('../tables/company');
 const AuthTable = require('../tables/auth');
-const {checkJwt} = require('../utils');
+
+if (!admin.apps.length) {
+  var serviceAccount = require(process.env.FIREBASE_ADMIN_KEY_PATH);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 const router = Router();
 
 //retrieve all jobs (later search/filter)
-router.get('/all/:user_guid', checkJwt, (req, res, next) => {
+router.post('/all/:user_guid', (req, res, next) => {
   const {user_guid} = req.params;
-  CompanyTable.getCompanies({user_guid})
+  const {fbIdToken} = req.body;
+
+  admin
+  .auth()
+  .verifyIdToken(fbIdToken)
+  .then((decodedToken) => {
+    const uid = decodedToken.uid;
+    
+    CompanyTable.getCompanies({user_guid})
     .then(resp => {
       if (resp.status_code === 401) {
         res.status(401).json({error: 'Please log in and try again.'})
@@ -17,10 +33,14 @@ router.get('/all/:user_guid', checkJwt, (req, res, next) => {
       }
     })
     .catch(err => next(err));
+  })
+  .catch((error) => {
+    next(error);
+  });
 })
 
-router.post('/', checkJwt, (req, res, next) => {
-  const {name, user_guid} = req.body;
+router.post('/', (req, res, next) => {
+  const {name, user_guid, fbIdToken} = req.body;
   const error_fields = [];
   const required_fields = ['name'];
 
@@ -33,23 +53,35 @@ router.post('/', checkJwt, (req, res, next) => {
     const error_message = `These fields are required: ${error_fields.join(', ')}`;
     res.status(400).json({message: error_message, type: 'error'})
   } else if (user_guid) {
-    CompanyTable.postCompany({name, user_guid})
-    .then(resp => {
-      if (resp.status_code === 401) {
-        res.status(401).json({error: 'Please log in and try again.'})
-      } else {
-        res.status(201).json(resp)
-      }
+    admin
+    .auth()
+    .verifyIdToken(fbIdToken)
+    .then((decodedToken) => {
+      const uid = decodedToken.uid;
+      CompanyTable.postCompany({name, user_guid})
+        .then(resp => {
+          if (resp.status_code === 401) {
+            res.status(401).json({error: 'Please log in and try again.'})
+          } else {
+            res.status(201).json(resp)
+          }
+        })
+        .catch(error => next(error))
+
     })
-    .catch(error => next(error))    
+    .catch((error) => {
+      next(error);
+      // Handle error
+    });
+     
   } else {
     res.status(200).json({message: 'Could not save job because there is an issue with the current user email authorization'})
   }
 });
 
-router.put('/update/:id', checkJwt, (req, res, next) => {
+router.put('/update/:id', (req, res, next) => {
   const id = req.params.id;
-  const {name, description, financial, purpose, user_guid} = req.body;
+  const {name, description, financial, purpose, user_guid, fbIdToken} = req.body;
   const error_fields = [];
   const required_fields = ['name'];
 
@@ -62,15 +94,27 @@ router.put('/update/:id', checkJwt, (req, res, next) => {
     const error_message = `These fields are required: ${error_fields.join(', ')}`;
     res.status(400).json({message: error_message, type: 'error'})
   } else if (id) {
-    CompanyTable.updateCompany({name, description, financial, purpose, id})
-    .then(resp => {
-      if (resp.status_code === 401) {
-        res.status(401).json({error: 'Please log in and try again.'})
-      } else {
-        res.status(201).json(resp)
-      }
-    })
-    .catch(error => next(error))    
+
+    admin
+      .auth()
+      .verifyIdToken(fbIdToken)
+      .then((decodedToken) => {
+        const uid = decodedToken.uid;
+        CompanyTable.updateCompany({name, description, financial, purpose, id})
+          .then(resp => {
+            if (resp.status_code === 401) {
+              res.status(401).json({error: 'Please log in and try again.'})
+            } else {
+              res.status(201).json(resp)
+            }
+          })
+          .catch(error => next(error))
+      })
+      .catch((error) => {
+        next(error);
+        // Handle error
+      });
+ 
   } else {
     res.status(200).json({message: 'Could not save company because there is an issue with the current user email authorization'})
   }
