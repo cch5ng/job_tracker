fc60cec5d3667daa0c460ff7116e2362f532c833
Carol Chung <cch5ng@gmail.com>
2021-10-24 12:58:33 -0700

    add help prop

--- client/src/components/FormShared/Input.tsx
+++ client/src/components/FormShared/Input.tsx
@@ -16,11 +16,12 @@ type InputProps = {
   required?: boolean;
   inputRef?: string;
   error?: boolean;
+  help?: string;
   inputOnChangeHandler(ev: React.FormEvent<HTMLInputElement>): void;
 }
 
 const Input = ({type, value, name, inputOnChangeHandler, label, inline, 
-  placeholder, inputRef, required, error, id}: InputProps) => {
+  placeholder, inputRef, required, error, id, help}: InputProps) => {
   const inputGroupClassName = cx(
     {
       inputGroupContainer: !inline,
@@ -42,6 +43,9 @@ const Input = ({type, value, name, inputOnChangeHandler, label, inline,
       )}
       {error && (
         <span>{error}</span>
+      )}
+      {help && (
+        <span>{help}</span>
       )}
       {value !== undefined && (
         <input type={type} value={value} name={name} 
